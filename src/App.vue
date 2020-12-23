bfafe72d7ee27fc8912736c770d3d4151efb6f13
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2020-12-23 22:27:17 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{title}}</h1>
+    <p>Something else</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Heyy Vue 3"
+    }
+  },
+}
+</script>
